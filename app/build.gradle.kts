73921e8cdd7bb7c5aaa1c7f4040f624bd2e7bb30
xonoxo143-ux <xonoxo143@gmail.com>
2026-04-25 13:43:53 -0400

    Add app module Gradle build file

---
+++ app/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.pocketbuilder"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.pocketbuilder"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
+}
+
+dependencies {
+}
