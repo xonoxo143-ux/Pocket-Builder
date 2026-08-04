bfbe8ace8215a1330fef930981591397f3bc292f
xonoxo143-ux <xonoxo143@gmail.com>
2026-08-04 08:21:32 -0400

    Configure legacy JNI packaging through Gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ android {
     }
 
     packaging {
+        jniLibs.useLegacyPackaging = true
         resources.excludes += setOf(
             "/META-INF/{AL2.0,LGPL2.1}",
             "/META-INF/LICENSE*",
