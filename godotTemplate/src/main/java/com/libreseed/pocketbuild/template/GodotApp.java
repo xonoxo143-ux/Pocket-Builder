c8324f2dde2b1ca8bc631f5c2e181050997ad138
xonoxo143-ux <xonoxo143@gmail.com>
2026-08-04 08:43:12 -0400

    Add generic Godot launcher activity

---
+++ godotTemplate/src/main/java/com/libreseed/pocketbuild/template/GodotApp.java
@@ -0,0 +1,7 @@
+package com.libreseed.pocketbuild.template;
+
+import org.godotengine.godot.GodotActivity;
+
+/** Generic launcher used as the base for PocketBuild-produced APKs. */
+public final class GodotApp extends GodotActivity {
+}
